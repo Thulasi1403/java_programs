481e4805fb1215524e2a72602efc916415288b7f
Thulasi1403 <92356867+Thulasi1403@users.noreply.github.com>
2022-04-09 16:20:05 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args)
+    {
+        int result;
+        Scanner s =new Scanner (System.in);
+        int a =s.nextInt();
+        int b =s.nextInt();
+        result  =(a+b);
+        System.out.println(result);
+}
+}
