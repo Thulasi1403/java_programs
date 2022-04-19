de6765df6efb367497c93d016e93b18d693d8065
Thulasi1403 <92356867+Thulasi1403@users.noreply.github.com>
2022-04-19 22:04:39 +0530

    Create SwapUsingArithmeticOperation.java

---
+++ SwapUsingArithmeticOperation.java
@@ -0,0 +1,19 @@
+
+import java .util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		a = a - b;
+		b = b-a; 
+b = b- a;
+System.out.println("The value of x is" + a + "and the value of y is "+ b );
+		
+
+	}
+
+}
