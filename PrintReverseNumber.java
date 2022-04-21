9aa7825aa608a525fe141a3b1f1251aecf70f820
Thulasi1403 <92356867+Thulasi1403@users.noreply.github.com>
2022-04-21 16:19:25 +0530

    Create PrintReverseNumber.java

---
+++ PrintReverseNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s =new Scanner (System.in);
+	    int a =s.nextInt();
+	    for (int i=a;i>=1;i--)
+	    {
+	        System.out.println(i);
+	    }
+	    
+	
+	}
+}
