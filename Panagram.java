259757e82ab245996fa90998978d90b65f7cb01d
Thulasi1403 <92356867+Thulasi1403@users.noreply.github.com>
2022-04-27 15:49:24 +0530

    Create Panagram.java

---
+++ Panagram.java
@@ -0,0 +1,32 @@
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		String str = "abcdefghijklmnopqrstuvwxyz";
+		boolean[] s = new boolean[26];
+		int index =0;
+		int flag =1;
+		for(int i=0;i<str.length();i++){
+		    if(str.charAt(i)>='A' && str.charAt(i)<='Z'){
+		        index =str.charAt(i) - 'A';
+		    }else if(str.charAt(i)>='a' && str.charAt(i)<='z'){
+		        index = str.charAt(i) - 'a';
+		    }
+		    s[index]=true;
+		}
+		for(int i=0;i<1;i++){
+		    if(s[i]==false){
+		        flag =0;
+		    }
+		    System.out.println("String :" + str);
+		    if (flag ==1){
+		        System.out.println("Panagram");
+		    }else{
+		        System.out.println("Not An Panagram");
+		    }
+		    
+		}
+		
+	}
+}
