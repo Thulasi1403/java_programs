900f054d976b286fc382b19c5880a0997ffc17ed
Thulasi1403 <92356867+Thulasi1403@users.noreply.github.com>
2022-05-01 20:02:20 +0530

    Create SortingArray.java

---
+++ SortingArray.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class Main
+{
+    
+	public static void main(String[] args) {
+		int[] arr = new int[]{13,2,45,34,87,12};
+		Arrays.sort(arr);
+		for(int i=0;i<arr.length;i++){
+		    System.out.println(arr[i]);
+		}
+	}
+
+}
+
